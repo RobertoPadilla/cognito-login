3574ecd4893e50a39f06348772fa8ef6e7e83039
Roberto Padilla (Linux) <basshivan981@gmail.com>
2022-08-19 18:22:56 -0500

    Creating database for application

---
+++ README.md
@@ -0,0 +1,18 @@
+# Cognito users app
+This app will validate if the user is authenticated in order to show their gallery that come from aws s3 bucket.
+
+## Development
+First we need to install the dependencies:
+```bash
+composer install
+```
+
+Then we need to create the database:
+```bash
+php spark db:create cognito_users
+```
+
+Then we need to migrate the database:
+```bash
+php spark migrate
+```
---
+++ app/Database/Migrations/2022-08-19-225528_InitDatabase.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class InitDatabase extends Migration
+{
+    public function up()
+    {
+        # Creating table users
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nickname' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'unique' => true,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+
+        # Creating table galleries
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'object_key' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('images');
+    }
+
+    public function down()
+    {
+        $this->forge->dropDatabase('cognito_users');
+    }
+}
